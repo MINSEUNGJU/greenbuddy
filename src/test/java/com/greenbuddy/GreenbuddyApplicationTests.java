ac09e409f5f9775bff40f5dffdc09f98f11c6e34
seungju <jooyful14@gmail.com>
2022-12-03 22:16:23 +0900

    init greenbuddy

---
+++ src/test/java/com/greenbuddy/GreenbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
